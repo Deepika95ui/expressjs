951e5a46b71cafb11a4f1499bb334af5cdcc2f5f
Deepika95ui <54905198+Deepika95ui@users.noreply.github.com>
2019-09-04 22:01:21 +0530

    Create flight status details application

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿const express = require('express')
+var cors = require('cors')
+const app = express()
+app.use(cors())
+
+const port = 3000
+const flights = require('./data/flights.js');
+app.get('/', function(req, res){
+  res.send('Hello World!')
+});
+app.get('/api/flights', cors(), function(req, res){
+   var response = [];
+    response = flights.filter(function(flightDetail){
+      if((flightDetail.flightNumber == req.query.fNumber || (flightDetail.origin == req.query.origin && flightDetail.destination == req.query.destination)) 
+    	 && flightDetail.departure == req.query.date){
+        return flightDetail;
+      }
+    });
+  res.send(response);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
